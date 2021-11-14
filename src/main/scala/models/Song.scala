003e159c4143898f0d1376e6dcbc3177f2ee9534
Amin Malekloo <amin.malekloo@yahoo.com>
2021-11-14 23:02:12 +0330

    fix (Song.scala): fixed fromRow static method, read from 4th column instead of 2nd column, overrided toString method for better iluustrations

--- src/main/scala/models/Song.scala
+++ src/main/scala/models/Song.scala
@@ -9,12 +9,15 @@ case class Song(
                  id: Long,
                  name: String,
                  artistName: Option[String]
-               )
+               ) {
+  override def toString: String =
+    s"Song(id=$id, name=$name, artistName = \"${artistName.getOrElse("Unknown artist")}\""
+}
 
 object Song {
   def fromRow(r: Row): Song = Song(
     id = r.getString(0).toLong,
     name = r.getString(1),
-    artistName = Try(r.getString(2)).toOption
+    artistName = Try(r.getString(4)).toOption
   )
 }
