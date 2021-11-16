384f66a191511dd6d0e94aaca728165caab15786
Amin Malekloo <amin.malekloo@yahoo.com>
2021-11-16 23:09:59 +0330

    feat: integrate recommender system with akka

--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -1,5 +1,22 @@
 package ir.ac.usc
 
+import controllers.{ApplicationStatusController, RecommendationController}
+
+import akka.util.Timeout
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
+import java.util.concurrent.TimeUnit
+
 object HttpServer {
+
+  implicit val timeout: Timeout = Timeout(60, TimeUnit.SECONDS)
+
+  private val applicationHandlerRoutes = ApplicationStatusController.routes
+  private val recommenerRoutes = RecommendationController.routes
+
+  val route: Route =
+    applicationHandlerRoutes ~
+      recommenerRoutes
 
 }
