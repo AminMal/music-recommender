7277f5e0f898f406ea99180df7e9119ac1c5f43f
Amin Malekloo <amin.malekloo@yahoo.com>
2021-11-21 00:10:45 +0330

    feat (Main.scala): removed modeling responsibility from Main to Context Manager Actor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,5 @@
 package ir.ac.usc
 
-import utils.ALSBuilder
-import Bootstrap.DataFrames.ratingsRDD
-import Bootstrap.system
-import conf.ALSDefaultConf
-import controllers.{ApplicationStatusController, RecommendationController}
-import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
-
-import scala.concurrent.Future
-
-
 object Main extends App {
-
-  import system.dispatcher
-  import HttpServer.{applicationController, recommenderActors}
-
-  HttpServer.serverBinding
-  val matrixModelFuture = Future {
-    val model: MatrixFactorizationModel = ALSBuilder.forConfig(ALSDefaultConf).run(ratingsRDD)
-    println(s"--- finished training model ---")
-    applicationController ! ApplicationStatusController.Messages.ModelActivated
-    recommenderActors.foreach { recommendActor =>
-      recommendActor ! RecommendationController.Messages.UpdateContext(model)
-    }
-    model
-  }
-
-
+  HttpServer.runServer()
 }
