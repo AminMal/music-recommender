7a00836e3e79a264831a394331d0cdd62483750c
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-15 02:25:50 +0330

    feat: service and handlers integration

--- src/main/scala/Bootstrap.scala
+++ src/main/scala/Bootstrap.scala
@@ -3,27 +3,22 @@ package ir.ac.usc
 import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{Level, Logger}
 import akka.actor.ActorSystem
-import controllers.ApplicationStatusController.Responses._
-import models.responses.{ErrorBody, FailureResponse, ResponseMessage, SuccessResponse}
-import models.{Song, SongDTO, User}
-
-import spray.json.DefaultJsonProtocol._
-import spray.json.RootJsonFormat
-import controllers.RecommendationController.Responses._
-
 import akka.stream.Materializer
-import controllers.ContextManagerActor.Messages.AddUserRating
+import server.RoutesModule
+import service.ServiceModule
 
-import conf.ALSConfig
+import com.typesafe.config.{Config, ConfigFactory}
 
 object Bootstrap {
+
+  val appConfig: Config = ConfigFactory.load()
 
   Logger.getLogger("org").setLevel(Level.ERROR)
 
   final val spark = SparkSession
     .builder()
     .appName("scommender")
-    .config("spark.master", "local[4]")  // todo, this needs to be removed in production
+    .config("spark.master", "local[8]")  // todo, this needs to be removed in production
     .getOrCreate()
 
   import utils.SparkFunctions._
@@ -34,27 +29,14 @@ object Bootstrap {
   println("--- initialized spark session ---")
 
   /**
-   * ActorSystem is requires to create actors and server
+   * ActorSystem is required to create actors and server
    */
-  implicit val system: ActorSystem = ActorSystem("scommender")
+  implicit val actorSystem: ActorSystem = ActorSystem("scommender")
   implicit val materializer: Materializer = Materializer.matFromSystem
 
-  object JsonImplicits {
-    implicit val modelActivationResponseFormatter: RootJsonFormat[ModelActivationResponse] = jsonFormat1(ModelActivationResponse)
-    implicit val healthCheckResponseFormatter: RootJsonFormat[HealthCheckResponse] = jsonFormat2(HealthCheckResponse)
-    implicit val songFormatter: RootJsonFormat[Song] = jsonFormat3(Song.apply)
-    implicit val recommendationResultFormatter: RootJsonFormat[RecommendationResult] = jsonFormat3(RecommendationResult)
-    implicit def successResponseFormatter[D](
-                                              implicit dataFormatter: RootJsonFormat[D]
-                                            ): RootJsonFormat[SuccessResponse[D]] =
-      jsonFormat2(SuccessResponse[D])
-
-    implicit val errorBodyFormatter: RootJsonFormat[ErrorBody] = jsonFormat2(ErrorBody.apply)
-    implicit def unsuccessResponseFormatter: RootJsonFormat[FailureResponse] = jsonFormat2(FailureResponse.apply)
-    implicit val addUserRatingFormatter: RootJsonFormat[AddUserRating] = jsonFormat3(AddUserRating.apply)
-    implicit val messageFormatter: RootJsonFormat[ResponseMessage] = jsonFormat1(ResponseMessage.apply)
-    implicit val addUserFormat: RootJsonFormat[User] = jsonFormat3(User.apply)
-    implicit val songDtoFormatter: RootJsonFormat[SongDTO] = jsonFormat6(SongDTO.apply)
-    implicit val alsConfigFormatter: RootJsonFormat[ALSConfig] = jsonFormat7(ALSConfig)
+  val services: ServiceModule = new ServiceModule {
+    override val system: ActorSystem = actorSystem
   }
+
+  val routesAndService: RoutesModule = new RoutesModule(services)
 }
--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -1,48 +1,28 @@
 package ir.ac.usc
 
-import controllers.{ApplicationStatusController, ConfigManagerActor, ContextManagerActor, PerformanceEvaluatorActor, RecommendationController, RecommenderManagerActor}
-
 import akka.http.scaladsl.Http
 import akka.util.Timeout
-import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
-import conf.ServerConfig
-
-import akka.actor.{ActorRef, Props}
-import controllers.RecommenderManagerActor.Messages._
-
-import akka.pattern.ask
-
 import scala.concurrent.Future
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.DurationInt
 
 object HttpServer {
 
-  import Bootstrap.{system, materializer}
   implicit val timeout: Timeout = Timeout(60, TimeUnit.SECONDS)
+  import Bootstrap.{actorSystem, materializer, routesAndService, appConfig}
+  import actorSystem.dispatcher
 
-  val route: Route =
-    ApplicationStatusController.routes ~
-      RecommendationController.routes ~
-      ContextManagerActor.routes
+  val interface: String = appConfig.getString("scommender.server.interface")
+  val port: Int         = appConfig.getInt("scommender.server.port")
 
-  import system.dispatcher
   lazy val runServer: () => Future[Http.ServerBinding] = () => Http().newServerAt(
-    ServerConfig.serverInterface, ServerConfig.serverPort
+    interface = interface, port = port
   )
     .withMaterializer(materializer)
-    .bind(route)
-    .map(_.addToCoordinatedShutdown(10.seconds))
-
-  println(s"--- started server on port ${ServerConfig.serverPort} ---")
-
-  val contextManagerActor: ActorRef = system.actorOf(Props[ContextManagerActor])
-  val recommenderManager: ActorRef = system.actorOf(Props[RecommenderManagerActor])
-  val applicationController: ActorRef = system.actorOf(Props[ApplicationStatusController])
-  val configManagerActor: ActorRef = system.actorOf(Props[ConfigManagerActor])
-  val performanceTestActor: ActorRef = system.actorOf(PerformanceEvaluatorActor.props)
-
-  def newRecommenderActor(): Future[ActorRef] =
-    (recommenderManager ? NewRecommenderActor).mapTo[ActorRef]
+    .bind(routesAndService.routes)
+    .map(_.addToCoordinatedShutdown(10.seconds))(actorSystem.dispatcher)
+    .map { binding =>
+      println(s"--- started server on port $port ---")
+      binding
+    }
 }
