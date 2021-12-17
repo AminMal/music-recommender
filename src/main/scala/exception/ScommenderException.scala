8d53ef84bb6108ecc3ee86c4771ebb0f9e87084a
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-18 01:38:56 +0330

    feat: added adopt method

--- src/main/scala/exception/ScommenderException.scala
+++ src/main/scala/exception/ScommenderException.scala
@@ -1,11 +1,20 @@
 package ir.ac.usc
 package exception
 
-import models.responses.FailureResponse
+import models.responses.{ErrorBody, FailureResponse}
 
 import akka.http.scaladsl.model.StatusCode
 
 trait ScommenderException extends Throwable {
   def toResponseBody: FailureResponse
   def status: StatusCode
+}
+
+object ScommenderException {
+  def adopt(throwable: Throwable): ScommenderException = new ScommenderException {
+    override def toResponseBody: FailureResponse =
+      FailureResponse(error = ErrorBody(code = 500, message = Some(throwable.getMessage.take(20))))
+
+    override def status: StatusCode = 500
+  }
 }
