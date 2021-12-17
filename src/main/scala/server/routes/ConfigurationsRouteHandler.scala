338fa2b3cedc1d7006ac69e4124fbb260cfec986
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-18 01:49:37 +0330

    minor improvements on routes for readability

--- src/main/scala/server/routes/ConfigurationsRouteHandler.scala
+++ src/main/scala/server/routes/ConfigurationsRouteHandler.scala
@@ -4,11 +4,11 @@ package server.routes
 import conf.ALSConfig
 import models.responses.SuccessResponse
 import service.algebra.ConfigurationManagementServiceAlgebra
+import utils.ApplicationJsonSupport
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-import utils.ApplicationJsonSupport
 
 
 class ConfigurationsRouteHandler(configService: ConfigurationManagementServiceAlgebra) {
@@ -16,17 +16,15 @@ class ConfigurationsRouteHandler(configService: ConfigurationManagementServiceAl
   import ConfigurationsRouteHandler._
 
   val routes: Route = pathPrefix("conf") {
-    path("update") {
-      put {
-        entity(as[ALSConfig]) { newConfig =>
-          parameter("force".as[Boolean].optional) { forced =>
-            configService.updateConfig(newConfig, forced.getOrElse(false))
+    (path("update") & put) {
+      entity(as[ALSConfig]) { newConfig =>
+        parameter("force".as[Boolean].optional) { forced =>
+          configService.updateConfig(newConfig, forced.getOrElse(false))
 
-            complete(
-              status = StatusCodes.OK,
-              SuccessResponse.forMessage("configurations updated successfully!")
-            )
-          }
+          complete(
+            status = StatusCodes.OK,
+            SuccessResponse.forMessage("configurations updated successfully!")
+          )
         }
       }
     }
