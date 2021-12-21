43d1e9ef014a5871939adf0b384e27d8d29c1f30
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-22 02:11:37 +0330

    feat: use SongDTO instead of Song model, removed unused methods

--- src/main/scala/utils/ResultParserImpl.scala
+++ src/main/scala/utils/ResultParserImpl.scala
@@ -2,21 +2,14 @@ package ir.ac.usc
 package utils
 
 import utils.DataFrames._
-import models.{Song, User}
+import models.{SongDTO, User}
+
+import org.apache.spark.mllib.recommendation.Rating
 
 import scala.util.Try
 import org.apache.spark.sql.functions._
 
 class ResultParserImpl extends ResultParser {
-
-  override def getSongInfo(songId: Int): Option[Song] = {
-    Try {
-      Song.fromRow(
-        songsDF.filter(_.getAs[Long]("song_id") == songId.toLong)
-          .head()
-      )
-    }.toOption
-  }
 
   override def getUserInfo(userId: Int): Option[User] =
     Try {
@@ -26,10 +19,10 @@ class ResultParserImpl extends ResultParser {
       )
     }.toOption
 
-  override def getSongs(songsIds: Seq[Int]): Seq[Song] = {
-    songsDF.filter(col("song_id") isin (songsIds: _*))
+  override def getSongDTOs(predictions: Seq[Rating]): Seq[SongDTO] = {
+    songsDF.filter(col("song_id") isin (predictions.map(_.product): _*))
       .collect().toSeq
-      .map(Song.fromRow)
+      .map(SongDTO.fromRow)
   }
 
 }
