c151c5128fa78c3ab1c103fbc2e09576e1be9a0c
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-22 02:03:20 +0330

    feat: Json snakecase support implementation

--- src/main/scala/utils/JsonSnakecaseFormatSupport.scala
+++ src/main/scala/utils/JsonSnakecaseFormatSupport.scala
@@ -1,5 +1,24 @@
-package ir.ac.usc.utils
+package ir.ac.usc
+package utils
 
-trait JsonSnakecaseFormatSupport {
+import spray.json.DefaultJsonProtocol
 
+
+trait JsonSnakecaseFormatSupport extends DefaultJsonProtocol {
+  import reflect._
+
+  override protected def extractFieldNames(classTag: ClassTag[_]): Array[String] = {
+    import java.util.Locale
+
+    def snakecase(name: String): String = PASS2.replaceAllIn(PASS1.replaceAllIn(name, REPLACEMENT), REPLACEMENT)
+      .toLowerCase(Locale.US)
+
+    super.extractFieldNames(classTag).map(snakecase)
+  }
+
+  private val PASS1 = """([A-Z]+)([A-Z][a-z])""".r
+  private val PASS2 = """([a-z\d])([A-Z])""".r
+  private val REPLACEMENT = "$1_$2"
 }
+
+object JsonSnakecaseFormatSupport extends JsonSnakecaseFormatSupport
