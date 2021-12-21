fbef7956a72a58c71f16c69f2d99192d1f79b4b9
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-22 02:14:45 +0330

    Bootstrap values renamed in HttpServer

--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -9,7 +9,7 @@ import scala.concurrent.duration.DurationInt
 object HttpServer {
 
   implicit val timeout: Timeout = Timeout(60, TimeUnit.SECONDS)
-  import Bootstrap.{actorSystem, materializer, routesAndService, appConfig}
+  import Bootstrap.{actorSystem, materializer, routes, appConfig}
   import actorSystem.dispatcher
 
   val interface: String = appConfig.getString("scommender.server.interface")
@@ -19,7 +19,7 @@ object HttpServer {
     interface = interface, port = port
   )
     .withMaterializer(materializer)
-    .bind(routesAndService.routes)
+    .bind(routes.routes)
     .map(_.addToCoordinatedShutdown(10.seconds))(actorSystem.dispatcher)
     .map { binding =>
       println(s"--- started server on port $port ---")
