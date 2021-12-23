b1953849fb463d75e6e4d1149e5909a418a8dcf8
Amin Malekloo <amin.malekloo@yahoo.com>
2021-12-23 23:43:40 +0330

    added models documentations

--- src/main/scala/models/responses/FailureResponse.scala
+++ src/main/scala/models/responses/FailureResponse.scala
@@ -1,6 +1,12 @@
 package ir.ac.usc
 package models.responses
 
+
+/**
+ * Represents failure http response
+ * @param success weather the operation was successful or not
+ * @param error error details
+ */
 case class FailureResponse(
                           success: Boolean = false,
                           error: ErrorBody
--- src/main/scala/models/responses/SuccessResponse.scala
+++ src/main/scala/models/responses/SuccessResponse.scala
@@ -1,12 +1,24 @@
 package ir.ac.usc
 package models.responses
 
+
+/**
+ * Represents successful http result
+ * @param success weather the operation was successful or not
+ * @param data actual response data
+ * @tparam D type of data parameter
+ */
 case class SuccessResponse[D](
                              success: Boolean = true,
                              data: D
                              )
 
 object SuccessResponse {
+  /**
+   * Creates a successful response for a string message
+   * @param message string message to return as body
+   * @return an object of successful response for the message
+   */
   def forMessage(message: String): SuccessResponse[ResponseMessage] =
     SuccessResponse(data = ResponseMessage(message))
 }
